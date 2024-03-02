d4522f5ca6733416a44f9acc1241d2a34bf4b9db
Ikazha <mr.dog.06@mail.ru>
2024-03-02 23:25:20 +0300

    удалил ненужные файлы и оптемезировал основной игровой цикл

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,30 +2,27 @@
 #include <vector>
 #include "Header.h"
 
-//#include "Deck.h"
-//#include "Player.h"
-//#include "Opponent_Bot.h"
-
-
-
-
 using namespace std;
-
 
 int main()
 {
 	setlocale(LC_ALL, "RU");
 	
-
 	Deck deck;
 	Player bob;
 	Bot_Opponent bot;
 
 	deck.shafl();
-	while (true)
+	for (int i = 0; i < 7; i++)
 	{
 		bob.take_card(deck);
+		bot.take_card(deck);
+	}
+	
+	while (true)
+	{
 		bob.zapros_card(bot);
+		bob.take_card(deck);
 		bot.take_card(deck);
 	    cout << endl << bob.score_plus() << "   " << bot.score_plus() << endl;
 	}
@@ -39,9 +36,8 @@ int main()
 
 // список требуемых функций:
 /*
-	1. передача карт из рук в руки
-	2. запрос карты игроком
-	3. запрос карты опонентом (прописать логику выбора карты)
-	4. прописать сробатывание фраз(мейби через выбор рандомной фразы)
+	
+	1. запрос карты опонентом (прописать логику выбора карты)
+	2. прописать сробатывание фраз(мейби через выбор рандомной фразы)
 	и тд..............
 */
